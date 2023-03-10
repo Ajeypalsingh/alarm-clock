1f44933404024d992a452685af4648206b798074
Ajaypal Singh <122804961+Ajeypalsingh@users.noreply.github.com>
2023-03-10 15:38:03 -0600

    added readme

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,6 +2,8 @@
 
 
 
+const displayTime = document.querySelector('.clock');
+
 
 function getClock() {
     const today = new Date();
@@ -9,10 +11,40 @@ function getClock() {
     let sec = today.getSeconds().toString().padStart(2, '0');
     let hours = today.getHours().toString().padStart(2, '0');
 
-    document.getElementById('hrs').innerHTML = hours;
-    document.getElementById('minutes').innerHTML = minute;
-    document.getElementById('seconds').innerHTML = sec;
-
+    displayTime.innerText = `${hours} : ${minute} : ${sec}`;
 }
 
 setInterval(getClock, 1000)
+
+const alarmSound = new Audio('./assets/audio/alarm-clock-sound.mp3');
+alarmSound.type = 'audio/mp3';
+
+const output = document.querySelector('h4');
+const input = document.querySelector("#alarmTime")
+const setAlarm = document.querySelector('.start-alarm')
+
+
+setAlarm.addEventListener('click', function () {
+    output.innerText = input.value;
+    const value = input.value;
+    
+
+    const inputTime = document.getElementById("alarmTime").value;
+    // Parse the input time into hours and minutes
+    const hoursMinutes = inputTime.split(":");
+    const hours = parseInt(hoursMinutes[0]);
+    const minutes = parseInt(hoursMinutes[1]);
+    
+    const currentDate = new Date();
+    const alarmDate = new Date();
+    alarmDate.setHours(hours, minutes, 0);
+    
+    let timeRemaining = alarmDate.getTime() - currentDate.getTime();
+    
+    // If the alarm time is in the past, add 24 hours to the alarm date
+    if (timeRemaining < 0) {
+        alarmDate.setDate(alarmDate.getDate() + 1);
+        timeRemaining = alarmDate.getTime() - currentDate.getTime();
+    }
+    setTimeout(() => { alarmSound.play() }, timeRemaining);
+});
